07fe344251a2df17689a4a49e4e599e96f50da60
sadiq <sadeg.r1@gmail.com>
2020-10-10 21:25:34 +0330

    Add  long_description_content_type='text/x-rst'

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     version='0.1.1',
     description='Tehran Stock Exchange OSINT Tool for Python',
     long_description=README,
+    long_description_content_type='text/x-rst',
     packages=['tfinance'],
     install_requires=["pandas", "requests", "bs4", "sqlalchemy", "tqdm"],
     url='https://github.com/sadeg/tfinance',
