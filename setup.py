e02fdaa8adbffe34ab26bc99c7009728a48e0964
sadiq <sadeg.r1@gmail.com>
2023-02-09 09:02:25 +0330

    FIX: Add subpackages to setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ README = (DIR / "README.rst").read_text()
 
 setup(
     name='tfinance',
-    version='0.1.3',
+    version='0.1.4',
     description='Tehran Stock Exchange OSINT Tool for Python',
     long_description=README,
     long_description_content_type='text/x-rst',
-    packages=['tfinance'],
-    install_requires=["pandas", "requests", "bs4", "sqlalchemy", "tqdm"],
+    packages=['tfinance', 'tfinance.models', 'tfinance.meta', 'tfinance.abc'],
+    install_requires=["pandas", "requests", "bs4", "sqlalchemy", "tqdm", "lxml"],
     url='https://github.com/sadeg/tfinance',
     license='GPL3',
     author='Sadiq Rahmati',
